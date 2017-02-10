826740c14b0f74be8862ae6dda3fd2a18dd30053
Naeem <Naeem>
2017-02-10 10:09:19 +0500

    Javascript Projects

---
+++ new projects/JavaScript/Memon federation/NaeeM/NaeeM/Class 1/app.js
@@ -0,0 +1,5 @@
+var a = prompt("Self Information", "Your Name");
+var b = prompt("Self Information", "Your F.Name");
+var c = prompt("Self Information", "Your Age");
+var d = prompt("Self Information", "Your Education");
+var e = alert(a +" " + b +" " + c +" " + d);
